42ff8d1de7536bc314f9eedf6d4226aba7134b2e
charles <charles@charlesdeMacBook-Air.local>
2025-05-22 19:15:02 +0800

    feat: 保存

--- .umirc.ts
+++ .umirc.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "umi";
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/charles/' : '/',
   publicPath: process.env.NODE_ENV === 'production' ? '/charles/' : '/',
+  history: {type: 'hash'},
   routes: [
     { path: "/", component: "index" },
     { path: "/docs", component: "docs" },
