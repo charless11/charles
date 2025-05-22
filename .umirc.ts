6237fe0d228927dfd516874a49a07314c6afac1a
charles <charles@charlesdeMacBook-Air.local>
2025-05-22 17:47:46 +0800

    feat: interview

--- .umirc.ts
+++ .umirc.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "umi";
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/charles/' : '/', // 项目站点需设置
+  publicPath: process.env.NODE_ENV === 'production' ? '/charles/' : '/',
+  history: { type: 'hash' }, // 避免 GitHub Pages 路由 404
   routes: [
     { path: "/", component: "index" },
     { path: "/docs", component: "docs" },
