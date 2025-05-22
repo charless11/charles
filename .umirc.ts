73df0cb824e6f068bbe64c781cb0d5468c436fee
charles <charles@charlesdeMacBook-Air.local>
2025-05-22 18:40:41 +0800

    Merge gh-pages updates

--- .umirc.ts
+++ .umirc.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "umi";
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/charles/' : '/', // 项目站点需设置
+  base: process.env.NODE_ENV === 'production' ? '/charles/' : '/',
   publicPath: process.env.NODE_ENV === 'production' ? '/charles/' : '/',
-  history: { type: 'hash' }, // 避免 GitHub Pages 路由 404
   routes: [
     { path: "/", component: "index" },
     { path: "/docs", component: "docs" },
